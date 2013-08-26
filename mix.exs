44ee6311168f79b48d1cb9b873525929f70c437e
Tim <tim@tim-MacBookAir.(none)>
2013-08-25 20:35:52 -0400

    remove GenFSM for 10.1

--- mix.exs
+++ mix.exs
@@ -1,10 +1,23 @@
+Code.append_path "deps/relex/ebin"
+
 defmodule Paxos.Mixfile do
   use Mix.Project
+
+  if Code.ensure_loaded?(Relex.Release) do
+    defmodule Release do
+      use Relex.Release
+
+      def name, do: "Paxos"
+      def version, do: Mix.project[:version]
+      def applications, do: [:paxos]
+      def lib_dirs, do: ["deps"]
+    end
+  end
 
   def project do
     [ app: :paxos,
       version: "0.0.1",
-      elixir: "~> 0.10.0",
+      elixir: "~> 0.10.1",
       deps: deps ]
   end
 
@@ -16,6 +29,6 @@ defmodule Paxos.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [{:relex, github: "yrashk/relex"}]
   end
 end
